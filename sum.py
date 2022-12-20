89cb2b84ce9d3c00a6670ed592c21e5805da67c3
Satyanaidugowthu <95331207+Satyanaidugowthu@users.noreply.github.com>
2022-12-20 23:12:39 +0530

    9-12-2022

---
+++ sum.py
@@ -0,0 +1,10 @@
+n1= int(input())
+n2= int(input())
+n3= int(input())
+n4= int(input())
+n5= int(input())
+sum=(n1+n2+n3+n4+n5)
+if ((n1<0) or (n2<0) or (n3<0) or (n4<0) or (n5<0)):
+    print("enter a positive number ")
+else:
+    print(sum)
